29dae6f46f7ef1cf0252e78d492e3769c972c976
Elena Kainarova <helenrizik@yandex.ru>
2023-08-20 00:07:20 +0300

    Добавить схемы и модели

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 const express = require('express');
 
+const mongoose = require('mongoose');
+
+const bodyParser = require('body-parser');
+
+const { PORT, DB_URL } = require('./utils/constant');
+
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Сервер запущен! Порт 3000');
+app.use(bodyParser.json());
+
+/* mongoose.connect(DB_URL); */
+
+mongoose.connect(DB_URL, () => {
+  console.log('БД успешно подключена');
+});
+
+app.listen(PORT, () => {
+  console.log(`Сервер запущен! Порт ${PORT}`);
 });
---
+++ models/user.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Поле "name" должно быть заполнено'],
+      minlength: [2, 'Минимальная длина поля "name" - 2'],
+      maxlength: [30, 'Максимальная длина поля "name" - 30'],
+    },
+
+    email: {
+      type: String,
+      required: [true, 'Поле "email" должно быть заполнено'],
+      unique: true,
+      validate: {
+        validator: (v) => validator.isEmail(v),
+        message: 'Некорректный email',
+      },
+    },
+    password: {
+      type: String,
+      required: [true, 'Поле "password" должно быть заполнено'],
+      minlength: [3, 'Минимальная длина поля "name" - 3'],
+      select: false, // поле select, чтобы API не возвращал хеш пароля
+    },
+
+  },
+  { versionKey: false },
+);
+
+module.exports = mongoose.model('user', userSchema);
