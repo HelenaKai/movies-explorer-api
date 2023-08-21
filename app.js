f9d50d1cc1aa0b344ade340b4c53f3eedfdbd76a
Elena Kainarova <helenrizik@yandex.ru>
2023-08-21 04:37:50 +0300

    Добавить logger

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const { errors } = require('celebrate');
 const router = require('./routes');
 
 const handleErrors = require('./middlewares/handleErrors');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
@@ -20,12 +21,11 @@ app.use(bodyParser.json());
 
 mongoose.connect(DB_URL);
 
-/* mongoose.connect(DB_URL, () => {
-  console.log('БД успешно подключена');
-});
- */
+app.use(requestLogger);
 
 app.use(router);
+
+app.use(errorLogger);
 
 app.use(errors());
 
